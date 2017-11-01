c3555ec156054ba1f2f77de5ad0e73d8114df260
gihanchanaka <gihanchanaka@gmail.com>
2017-11-01 19:58:39 +0530

    Check into toDebug 01

---
+++ ToDebug/01/working.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+sum=np.ndarray(shape=(100,480, 848, 3),dtype=int)
+
+for x in range(100):
+    ret,frame=cap.read()
+    sum[x]=frame.copy()
+    cv2.imshow('ThisFrame',frame)
+    k = cv2.waitKey(30) & 0xff
+
+
+cap.release()
+cv2.destroyAllWindows()
