0d0a40b8a3c1b7856e5ad3c58390fabc10dfbc9a
gihanchanaka <gihanchanaka@gmail.com>
2017-11-03 13:30:44 +0530

    Just commiting before I start with the GMM

--- ToDebug/01/notWorking.py
+++ ToDebug/01/notWorking.py
@@ -1,15 +1,17 @@
+
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
 
-sum=np.ndarray(shape=(100,480, 848, 3),dtype=int)
+ssum=np.ndarray(shape=(100,480, 848, 3),dtype=int)
 
 for x in range(100):
     ret,frame=cap.read()
-    sum[x]=frame.copy()
-    cv2.imshow('ThisFrame',sum[x])
+    ssum[x]=frame
+    print(ssum[x])
+    cv2.imshow('ThisFrame',ssum)
     k = cv2.waitKey(30) & 0xff
 
 
